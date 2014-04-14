1849c0a18cda76c70e11bb498343d7d34458a65a
luisvelascoserpis <luis.velasco.serpis@gmail.com>
2014-04-14 13:50:19 +0200

    Añadido PHolaMundoNb

---
+++ PHolaMundoNb/src/pholamundonb/PHolaMundoNb.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pholamundonb;
+
+/**
+ *
+ * @author luisvelasco
+ */
+public class PHolaMundoNb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //System.out.println("Hola Mundo");
+        
+    }
+}
